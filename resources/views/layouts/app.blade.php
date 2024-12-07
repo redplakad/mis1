0bffb738652b5f2556804d220093b4ac9dfd5c19
redplakad <redplakad@gmail.com>
2024-12-07 23:59:57 +0700

    init projet

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ env("APP_TITLE") }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Styles -->
+        @include('partials.styles')
+    </head>
+    <body class="boxed-size">
+        @include('partials.preloader')
+        @include('partials.sidebar')
+
+        <div class="container-fluid">
+			<div class="main-content d-flex flex-column">
+				<!-- Start Header Area -->
+				@include('partials.header')
+				<!-- End Header Area -->
+
+                @yield('content')
+				<div class="flex-grow-1"></div>
+
+				<!-- Start Footer Area -->
+				@include('partials.footer')
+				<!-- End Footer Area -->
+			</div>
+		</div>
+
+        
+        @include('partials.theme_settings')
+        @include('partials.scripts')
+    </body>
+</html>
---
+++ resources/views/partials/preloader.blade.php
@@ -0,0 +1,16 @@
+<div class="preloader" id="preloader">
+    <div class="preloader">
+        <div class="waviy position-relative">
+            <span class="d-inline-block">B</span>
+            <span class="d-inline-block">P</span>
+            <span class="d-inline-block">R</span>
+            <span class="d-inline-block">-</span>
+            <span class="d-inline-block">S</span>
+            <span class="d-inline-block">E</span>
+            <span class="d-inline-block">R</span>
+            <span class="d-inline-block">A</span>
+            <span class="d-inline-block">N</span>
+            <span class="d-inline-block">G</span>
+        </div>
+    </div>
+</div>
