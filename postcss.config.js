40ba5e63015208e331030706ae1ffa6eb381e18b
redplakad <redplakad@gmail.com>
2024-12-08 21:57:04 +0700

    initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 export default {
     plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
+      'tailwindcss/nesting': 'postcss-nesting',
+      tailwindcss: {},
+      autoprefixer: {},
     },
-};
+  }
+  
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');    
-})->middleware(['auth', 'verified'])->name('root');;
+})->middleware(['auth', 'verified'])->name('root');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
